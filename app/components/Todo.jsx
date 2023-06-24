cd69181b1a245cc842e7b3b877a3b32886065e10
Medetcan Diler <medetdiler1996@gmail.com>
2023-06-24 16:55:24 +0300

    Updated Todo Component

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,17 @@
 import { FaRegTrashAlt, FaEdit } from "react-icons/fa";
 
+import Loader from "./Loader";
+
 const style = {
-  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  li: `relative flex justify-between bg-slate-200 p-4 my-2 capitalize`,
   liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
-  row: `flex `,
+  row: `flex items-center `,
   checkbox: `cursor-pointer`,
   text: `ml-2 cursor-pointer`,
   textComplete: `ml-2 cursor-pointer line-through`,
   button: `cursor-pointer hover:text-red-500`,
   editButton: `cursor-pointer hover:opacity-75`,
-  activeEditButton: `cursor-pointer, opacity-50`,
+  activeEditButton: `cursor-pointer animate-bounce opacity-50`,
   btnContainer: `flex items-center space-x-1`,
 };
 
@@ -29,9 +31,15 @@ function Todo({ todo, handleToggle, handleDelete, handleEdit }) {
         >
           {todo.text}
         </p>
+        <div className="absolute left-1/2">{todo.onEdit && <Loader />}</div>
       </div>
       <div className={style.btnContainer}>
-        <button onClick={() => handleEdit(todo)} className={todo.onEdit ? style.activeEditButton : style.editButton}>{<FaEdit />}</button>
+        <button
+          onClick={() => handleEdit(todo)}
+          className={todo.onEdit ? style.activeEditButton : style.editButton}
+        >
+          {<FaEdit />}
+        </button>
         <button className={style.button} onClick={() => handleDelete(todo)}>
           {<FaRegTrashAlt />}
         </button>
