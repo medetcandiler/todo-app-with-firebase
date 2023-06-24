a751e544c31560d2ff4daa06781ca6c736af3c01
Medetcan Diler <medetdiler1996@gmail.com>
2023-06-24 17:31:48 +0300

    Updated title

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'To do app',
+  title: 'Todo app',
   description: 'Created by Medetcan Diler',
 }
 
